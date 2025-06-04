96555b85b9734e68f6a26a3524c69d23c63ca2a2
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2025-06-04 16:38:44 +0300

    upd pixabay

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = `50666263-41be0f694eb808efe4a214ed6`
-export function getFetchImg(searchQuery) {
+export async function getFetchImg(searchQuery) {
     const params = new URLSearchParams({
       key: API_KEY,
       q: searchQuery,
@@ -11,10 +11,7 @@ export function getFetchImg(searchQuery) {
       safesearch: true,
     });
   
-    return fetch(`${BASE_URL}?${params}`).then(response => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    });
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
   }
+  
